c55c467d11622d34d9c68bb5ac142eda6cd54a05
Yao Wu <2395031@edu.vaniercollege.qc.ca>
2024-02-15 14:49:21 -0500

    login component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {CoursewebComponent} from './course-web.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'courses',
+        component : CoursewebComponent,
+    }
+];
--- frontend/src/app/course-web.component.ts
+++ frontend/src/app/course-web.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from "@angular/core";  // an Angular decorate
 
 @Component({
     selector: 'courses', // <div id='courses'> #courses    <courses> (extend html element)
     template: '<h2>Courses</h2>'
 })
+
 export class CoursewebComponent{
 
 }
