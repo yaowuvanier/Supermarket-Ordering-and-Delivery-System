08f7ebec9bc0fc11f23e42fdabbc91a9560b4698
Yao Wu <2395031@edu.vaniercollege.qc.ca>
2024-02-20 15:05:08 -0500

    bootstrap-icons and star star-fill click

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {CoursewebComponent} from './course-web.component'
 import { LoginComponent } from './pages/admin/login/login.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,10 @@ export const routes: Routes = [
     {
         path: 'authors',
         component: AuthorsComponent
-    }
+    },
+    {
+        path: 'favorite',
+        component: FavoriteComponent
+    },
+    
 ];
--- frontend/src/app/favorite/favorite.component.ts
+++ frontend/src/app/favorite/favorite.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
-  standalone: true,
-  imports: [],
   templateUrl: './favorite.component.html',
-  styleUrl: './favorite.component.scss'
+  styleUrls: ['./favorite.component.scss']
 })
 export class FavoriteComponent {
+  isFavorite = false;
 
+  onClick() {
+    this.isFavorite = !this.isFavorite;
+  }
 }
