dfd225e20a178c8097179e6c353318930e7ebc92
Yao Wu <yao.wu.mtl@gmail.com>
2024-03-12 16:24:58 -0400

    generate some component like check-out homw products shopping-cart

---
+++ frontend/src/app/check-out/check-out.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-out',
+  standalone: true,
+  imports: [],
+  templateUrl: './check-out.component.html',
+  styleUrl: './check-out.component.scss'
+})
+export class CheckOutComponent {
+
+}
