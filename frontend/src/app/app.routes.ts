5c862286f4439ecc0c261a0ce6d2a9a9ef48fbd6
Yao Wu <yao.wu.mtl@gmail.com>
2024-03-12 16:48:59 -0400

    adding route infor

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,40 +1,34 @@
 import { Routes } from '@angular/router';
-import {CoursewebComponent} from './course-web.component'
-import { LoginComponent } from './pages/admin/login/login.component';
-import { AuthorsComponent } from './authors/authors.component';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { TitleCaseComponent } from './title-case/title-case.component';
-import { DIRECTIVEComponent } from './directive/directive.component';
+// import {CoursewebComponent} from './course-web.component'
+// import { LoginComponent } from './pages/admin/login/login.component';
+// import { AuthorsComponent } from './authors/authors.component';
+// import { FavoriteComponent } from './favorite/favorite.component';
+// import { TitleCaseComponent } from './title-case/title-case.component';
+// import { DIRECTIVEComponent } from './directive/directive.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { LoginComponent } from './login/login.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 export const routes: Routes = [
-    {
-        path : '',
-        redirectTo: 'login',
-        pathMatch : 'full',
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'courses',
-        component: CoursewebComponent
-    },
-    {
-        path: 'authors',
-        component: AuthorsComponent
-    },
-    {
-        path: 'favorite',
-        component: FavoriteComponent
-    },
-    {
-        path: 'TitleCase',
-        component: TitleCaseComponent       
-    },
-    {
-        path: 'dir',
-        component: DIRECTIVEComponent       
-    }
+    // {
+    //     path : '',
+    //     redirectTo: 'login',
+    //     pathMatch : 'full',
+    // },
+    {  path: '',  component: HomeComponent },
+    {  path: 'products',  component: ProductsComponent },
+    {  path: 'shopping-cart',  component: ShoppingCartComponent },
+    {  path: 'check-out',  component: CheckOutComponent },
+    {  path: 'order-success',  component: OrderSuccessComponent },
+    {  path: 'login',  component: LoginComponent },
+    {  path: 'admin/produts',  component: AdminProductsComponent },   
+    {  path: 'admin/orders',  component: AdminOrdersComponent },
+    {  path: '',  component: HomeComponent },
+
     
 ];
