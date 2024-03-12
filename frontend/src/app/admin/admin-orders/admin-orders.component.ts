4e15a170309bc4916a0ccb4db5f91e47d4cc5dd0
Yao Wu <yao.wu.mtl@gmail.com>
2024-03-12 16:27:56 -0400

    sdding some admin component

---
+++ frontend/src/app/admin/admin-orders/admin-orders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-orders.component.html',
+  styleUrl: './admin-orders.component.scss'
+})
+export class AdminOrdersComponent {
+
+}
