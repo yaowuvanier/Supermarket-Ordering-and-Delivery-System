a34aa2028e508694a2e19ae4394b40842909ea50
Yao Wu <yao.wu.mtl@gmail.com>
2024-03-17 19:17:50 -0400

    adding a google login button

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/app';
+import { getAuth , GoogleAuthProvider} from 'firebase/auth';
+import 'firebase/compat/firestore';
+
 
 @Component({
   selector: 'app-login',
@@ -9,4 +14,18 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private afAuth : AngularFireAuth){
+
+  }
+
+  async login() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await this.afAuth.signInWithPopup(provider);
+      console.log(result.user);
+    } catch (error) {
+      console.error('Error signing in with Google:', error);
+    }
+  }
 }
