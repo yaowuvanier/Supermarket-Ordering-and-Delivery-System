65f72e7e16bac0c351af19ea3ea8341ef1320493
Yao Wu <yao.wu.mtl@gmail.com>
2024-03-28 13:46:13 -0400

    adding a notFound componnet

--- frontend-ng/src/app/app.module.ts
+++ frontend-ng/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
