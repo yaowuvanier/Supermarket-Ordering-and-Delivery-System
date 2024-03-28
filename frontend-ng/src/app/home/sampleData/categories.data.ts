bdd9fd6cf76bfc5af962728e0a2ddba0ae41c45d
Yao Wu <yao.wu.mtl@gmail.com>
2024-03-28 14:12:04 -0400

    adding data about category, now use fake data, later using from database

---
+++ frontend-ng/src/app/home/sampleData/categories.data.ts
@@ -0,0 +1,10 @@
+import { Category } from "../types/category.type";
+
+export const categories: Category[] = [
+    { id:1,category: "All Categories"},
+    { id:2, category: "Bread"},
+    { id:3, category: "Dairy"},
+    { id:4, category: "Fruits"},
+    { id:5, category: "Seasonings and Spices"},
+    { id:6, category: "Vegetables"}
+]
