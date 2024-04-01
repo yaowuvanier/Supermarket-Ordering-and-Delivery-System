10137032b192eccb4dc09318b4828ac6d8dd6f82
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-01 13:48:37 -0400

    adding prodcuts data

---
+++ frontend-ng/src/app/home/components/products/products.data.ts
@@ -0,0 +1,117 @@
+import { ProductListItem } from "./products.type";
+
+const product: ProductListItem =[
+    {
+        id: 1,
+        product_name: 'bread1',
+        product_img: 'bread1.jpg',
+        price: 4,
+        category: 'Bread'
+    },
+    {
+        id: 2,
+        product_name: 'bread2',
+        product_img: 'bread2.jpg',
+        price: 5,
+        category: 'Bread'
+    },
+    {
+        id: 3,
+        product_name: 'bread3',
+        product_img: 'bread3.jpg',
+        price: 5,
+        category: 'Bread'
+    },
+    {
+        id: 4,
+        product_name: 'cheese',
+        product_img: 'cheese.jpg',
+        price: 10,
+        category: 'Dairy'
+    },    
+    {
+        id: 5,
+        product_name: 'bread1',
+        product_img: 'milk.jpg',
+        price: 4,
+        category: 'Dairy'
+    },    
+    {
+        id: 6,
+        product_name: 'apple',
+        product_img: 'apple.jpg',
+        price: 2,
+        category: 'Fruits'
+    },
+    {
+        id: 7,
+        product_name: 'avocado',
+        product_img: 'avocado.jpg',
+        price: 5,
+        category: 'Fruits'
+    },
+    {
+        id: 8,
+        product_name: 'blueberry',
+        product_img: 'blueberry.jpg',
+        price: 4.99,
+        category: 'Fruits'
+    },
+    {
+        id: 9,
+        product_name: 'cherry',
+        product_img: 'cherry.jpg',
+        price: 5.99,
+        category: 'Fruits'
+    },
+    {
+        id: 10,
+        product_name: 'Grape',
+        product_img: 'Grape.jpg',
+        price: 2.99,
+        category: 'Fruits'
+    },
+    {
+        id: 11,
+        product_name: 'basil',
+        product_img: 'basil.jpg',
+        price: 5.99,
+        category: 'Seasonings and Spices'
+    },
+    {
+        id: 12,
+        product_name: 'cinnamon',
+        product_img: 'cinnamon.jpg',
+        price: 8.99,
+        category: 'Seasonings and Spices'
+    },
+    {
+        id: 13,
+        product_name: 'dried-chili-pepper',
+        product_img: 'dried-chili-pepper.jpg',
+        price: 5.99,
+        category: 'Seasonings and Spices'
+    },
+    {
+        id: 14,
+        product_name: 'broccoli',
+        product_img: 'broccoli.jpg',
+        price: 3.99,
+        category: 'Vegetables'
+    },   
+    {
+        id: 15,
+        product_name: 'carrot',
+        product_img: 'carrot.jpg',
+        price: 1.99,
+        category: 'Vegetables'
+    },    
+    {
+        id: 16,
+        product_name: 'corn',
+        product_img: 'corn.jpg',
+        price: 0.99,
+        category: 'Vegetables'
+    }
+]
+    
