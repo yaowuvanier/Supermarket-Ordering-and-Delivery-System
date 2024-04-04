5d62c670002505737baae791aee4439605fb0d85
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-04 14:53:40 -0400

    SidenavigationComponent : the category data get from storeItem which get from categoryService

--- frontend-ng/src/app/home/components/sider-navigation/sider-navigation.component.ts
+++ frontend-ng/src/app/home/components/sider-navigation/sider-navigation.component.ts
@@ -1,26 +1,33 @@
+import { Component,OnDestroy} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CategoriesStoreItem } from '../../services/categories.storeItem';
 import { CategoryService } from './../../services/category.service';
 import { categories } from './../../sampleData/categories.data';
 import { Category } from './../../types/category.type';
-import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sider-navigation',
   templateUrl: './sider-navigation.component.html',
   styleUrls: ['./sider-navigation.component.scss']
 })
-export class SiderNavigationComponent {
-  categories:Category[] = [];
-  constructor (categoryService : CategoryService){
-    // Fetch categories from the CategoryService
-    //subscrible--Observable through apiURL
-   categoryService.getAllCategories().subscribe((categories)=>{
-    this.categories = categories;
-   });
+// 
+export class SidenavigationComponent implements OnDestroy {
+  categories: Category[] = [];
+  subscriptions: Subscription = new Subscription();
+
+  constructor(categoryStore: CategoriesStoreItem) {
+    this.subscriptions.add(
+      categoryStore.categories$.subscribe((categories) => {
+        this.categories = categories;
+      })
+    );
   }
 
-  getCategorie (categoryName: string) : Category[]{
-    return this.categories.filter(
-      (category) => categoryName = category.category
-    )
+  getCategories(): Category[] {
+    return this.categories;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
--- frontend-ng/src/app/home/home.module.ts
+++ frontend-ng/src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SiderNavigationComponent } from './components/sider-navigation/sider-navigation.component';
+import { SidenavigationComponent } from './components/sider-navigation/sider-navigation.component';
 import { ProductsComponent } from './components/products/products.component';
 
 import { HttpClientModule } from '@angular/common/http'
@@ -15,7 +15,7 @@ import { CategoriesStoreItem } from './services/categories.storeItem';
   declarations: [
     HomeComponent,
     HeaderComponent,
-    SiderNavigationComponent,
+    SidenavigationComponent,
     ProductsComponent
   ],
   imports: [
