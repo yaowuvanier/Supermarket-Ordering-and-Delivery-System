4827b39e641210ed77f964e9cb438718aa2980da
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-04 12:29:45 -0400

    adding a rating component

--- frontend-ng/src/app/shared/shared.module.ts
+++ frontend-ng/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RatingsComponent } from './components/ratings/ratings.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RatingsComponent
+  ],
   imports: [
     CommonModule
   ]
