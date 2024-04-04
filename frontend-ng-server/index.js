f54a4b5f1612dc6a2bdd2380710e74b7153818a0
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-04 05:37:36 -0400

    using app.use(), invoke the get api

--- frontend-ng-server/index.js
+++ frontend-ng-server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
 const PORT = 5001;
+const productCategories = require('./routes/productCategories');
+
+app.use('/productCategories',productCategories);
 
 const server = app.listen(PORT, () =>
     console.log('APP IS RUNNING ON THE PORT - 5001...')
