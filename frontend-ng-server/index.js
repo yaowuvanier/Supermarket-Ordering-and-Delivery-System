0211581049b24a9f69870f1b0a2de635f5bde64e
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-04 12:28:28 -0400

    adding a module named shared

--- frontend-ng-server/index.js
+++ frontend-ng-server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const PORT = 5001;
 const productCategories = require('./routes/productCategories');
 
+app.use(cors());
 app.use('/productCategories',productCategories);
+
 
 const server = app.listen(PORT, () =>
     console.log('APP IS RUNNING ON THE PORT - 5001...')
