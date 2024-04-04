612b7a826956b60f80eaa51e0adf3e3e459eef17
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-04 05:34:10 -0400

    crate routes folder and set route specify

--- frontend-ng-server/index.js
+++ frontend-ng-server/index.js
@@ -1,33 +1,7 @@
 const express = require("express");
 const app = express();
-const mysql = require('mysql2');
+
 const PORT = 5001;
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'estore1',
-    port: 3306,
-    multipleStatements: true
-});
-
-app.get('/', (req, res) => {
-    /*no need getConnection method, direct using pool.query, because pool will handle the connet error */
-    // pool.getConnection((err, connection)=>{
-    //     if(err)
-    //         res.status(500).send(err);
-    //     else{
-            pool.query('select* from categories', (error, categories)=>{
-                if (error)
-                    res.status(500).send(error);
-                else
-                    res.status(200).send(categories);
-            })
-    //     }
-            
-    // })
-})
 
 const server = app.listen(PORT, () =>
     console.log('APP IS RUNNING ON THE PORT - 5001...')
---
+++ frontend-ng-server/routes/productCategories.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const productCategories = express();
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'estore',
+    port: 3306,
+    multipleStatements: true
+});
+
+productCategories.get('/', (req, res) => {
+    /*no need getConnection method, direct using pool.query, because pool will handle the connet error */
+    // pool.getConnection((err, connection)=>{
+    //     if(err)
+    //         res.status(500).send(err);
+    //     else{
+            pool.query('select* from categories', (error, categories)=>{
+                if (error)
+                    res.status(500).send(error);
+                else
+                    res.status(200).send(categories);
+            })
+    //     }
+            
+    // })
+})
+
+module.exports = productCategories
