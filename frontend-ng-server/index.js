9ce531204f7a214e42f884f6cb4c6f8a89d87558
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-05 02:24:33 -0400

    adding users api route

--- frontend-ng-server/index.js
+++ frontend-ng-server/index.js
@@ -4,12 +4,15 @@ const app = express();
 const PORT = 5001;
 const productCategories = require('./routes/productCategories');
 const products = require('./routes/products');
-
+const users = require('./routes/users');
+const bodyparser = require('body-parser');
 
 
 app.use(cors());
+app.use(bodyparser.json());
 app.use('/productCategories',productCategories);
 app.use('/products', products )
+app.use('/users', users )
 
 const server = app.listen(PORT, () =>
     console.log('APP IS RUNNING ON THE PORT - 5001...')
