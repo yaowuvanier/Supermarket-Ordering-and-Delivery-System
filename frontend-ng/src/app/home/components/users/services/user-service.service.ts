a3ffc2a8668e2946b9a5238696d7114d4146e166
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-06 16:13:34 -0400

    create a user when submit the form

--- frontend-ng/src/app/home/components/users/services/user-service.service.ts
+++ frontend-ng/src/app/home/components/users/services/user-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { user } from 'src/app/home/types/user.type';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserServiceService {
+@Injectable()
+export class UserService  {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  createUser(user: user): Observable<any> {
+    const url: string = 'http://localhost:5001/users/signup';
+    return this.httpClient.post(url, user);
+  }
+
 }
--- frontend-ng/src/app/home/components/users/user-signup/user-signup.component.ts
+++ frontend-ng/src/app/home/components/users/user-signup/user-signup.component.ts
@@ -1,16 +1,25 @@
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { matchPasswords } from './validators/match-passwords.validator'
 import {passwordStrength} from './validators/password-Strength.validator'
 
+import { UserService } from '../services/user-service.service';
+import { user } from 'src/app/home/types/user.type';
+
+
 @Component({
   selector: 'app-user-signup',
   templateUrl: './user-signup.component.html',
   styleUrls: ['./user-signup.component.scss'],
+  providers: [UserService]
 })
 export class UserSignupComponent implements OnInit {
   userSignupForm: FormGroup | undefined;
-  constructor(private fb: FormBuilder) {}
+  alertMessage: string = '';
+  alertType: number = 0; // 0-success, 1-warning, 2-error
+
+  constructor(private fb: FormBuilder, private userService : UserService) {}
   ngOnInit(): void {
     this.userSignupForm = this.fb.group({
       firstName: ['', Validators.required],
@@ -45,5 +54,31 @@ export class UserSignupComponent implements OnInit {
   }
 
 
-  onSubmit(): void {}
+  onSubmit(): void {
+    const user: user = {
+      firstName: this.firstName?.value,
+      lastName: this.userSignupForm.get('lastName')?.value,
+      address: this.userSignupForm.get('address')?.value,
+      city: this.userSignupForm.get('city')?.value,
+      state: this.userSignupForm.get('state')?.value,
+      pin: this.userSignupForm.get('pin')?.value,
+      email: this.userSignupForm.get('email')?.value,
+      password: this.userSignupForm.get('password')?.value,
+    };
+    this.userService.createUser(user).subscribe({
+      next: (result) => {
+        if (result.message === 'success') {
+          this.alertMessage = 'User created successfully';
+          this.alertType = 0;
+        } else if (result.message === 'Email already exists') {
+          this.alertMessage = result.message;
+          this.alertType = 1;
+        }
+      },
+      error: (error) => {
+        this.alertMessage = error.message;
+        this.alertType = 2;
+      },
+    });
+  }
 }
