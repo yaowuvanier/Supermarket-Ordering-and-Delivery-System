94596098b7df169baa18fbadd6fb11cad3c09745
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-07 12:52:43 -0400

    finish login component and register router jumping

--- frontend-ng/src/app/app-routing.module.ts
+++ frontend-ng/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { CartComponent } from './home/components/cart/cart.component';
 import { UserSignupComponent } from './home/components/users/user-signup/user-signup.component';
+import { UserLoginComponent } from './home/components/users/user-login/user-login.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'cart', component:CartComponent},
   {path: 'signup', component:UserSignupComponent},
+  {path: 'login', component:UserLoginComponent},
   
   {path: '**', component:NotFoundComponent},
 
