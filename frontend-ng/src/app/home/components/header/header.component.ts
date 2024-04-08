4b97b2f4166f9e488e755a9e50978f7eeb9143df
Yao Wu <yao.wu.mtl@gmail.com>
2024-04-07 20:01:23 -0400

    adds subscriptions to the userService Observables

--- frontend-ng/src/app/home/components/header/header.component.ts
+++ frontend-ng/src/app/home/components/header/header.component.ts
@@ -1,22 +1,41 @@
-import { Component, EventEmitter,Output } from '@angular/core';
+import { Component, EventEmitter,Output, OnDestroy } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // import { SearchKeyword } from '../../types/searchKeyword.type';
 import { filter } from 'rxjs/operators';
 import { CartStoreItem } from '../../services/cart/cart.storeItem';
 import { CategoriesStoreItem } from '../../services/categories.storeItem';
-
+import { UserService } from '../users/services/user-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy{
+
+  subscriptions: Subscription = new Subscription();
+  isUserAuthenticated: boolean = false;
+  userName: string = '';
 
   constructor(
     public categoryStore: CategoriesStoreItem,
-    public cartStore: CartStoreItem
-  ){}
+    public cartStore: CartStoreItem,
+    public userService: UserService
+  ){
+    this.subscriptions.add(
+      this.userService.isUserAuthenticated$.subscribe((result) => {
+        this.isUserAuthenticated = result;
+      })
+    );
+
+    this.subscriptions.add(
+      this.userService.loggedInUser$.subscribe((result) => {
+        this.userName = result.firstName;
+      })
+    );
+  }
+  
   @Output()
   searchClicked: EventEmitter<string> = new EventEmitter<string>();
   keyword: string = '';
--- frontend-ng/src/app/home/components/users/services/user-service.service.ts
+++ frontend-ng/src/app/home/components/users/services/user-service.service.ts
@@ -22,6 +22,9 @@ export class UserService  {
     return this.isAuthenticated.asObservable();
   }
 
+  get loggedInUser$(): Observable<loggedInUser> {
+    return this.loggedInUserInfo.asObservable();
+  }
   createUser(user: user): Observable<any> {
     const url: string = 'http://localhost:5001/users/signup';
     return this.httpClient.post(url, user);
@@ -38,5 +41,14 @@ export class UserService  {
       'expiry',
       new Date(Date.now() + token.expiresInSeconds * 1000).toISOString()
     );
+    localStorage.setItem('firstName', token.user.firstName);
+    localStorage.setItem('lastName', token.user.lastName);
+    localStorage.setItem('address', token.user.address);
+    localStorage.setItem('city', token.user.city);
+    localStorage.setItem('state', token.user.state);
+    localStorage.setItem('pin', token.user.pin);
+
+    this.isAuthenticated.next(true);
+    this.loggedInUserInfo.next(token.user);
   }
 }
